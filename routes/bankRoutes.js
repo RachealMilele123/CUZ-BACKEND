3bcaaf96f51d75e641e49b11d180fdc823889a48
Desire <desireirankundawilliams@gmail.com>
2025-11-02 21:03:50 +0200

    able to send an email forgot password

--- routes/bankRoutes.js
+++ routes/bankRoutes.js
@@ -8,6 +8,8 @@ const registerController = require("../controllers/registerController");
 
 router.post("/login", authControllers.login);
 router.post("/register", registerController.register);
+router.post("/forgot-password", authControllers.forgotPassword);
+router.post("/reset-password/:token", authControllers.resetPassword);
 router.post("/approve", bankControllers.approveUser);
 router.post("/deposit", bankControllers.deposit);
 router.post("/transfer", bankControllers.transfer);
